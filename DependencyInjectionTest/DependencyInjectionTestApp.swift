192b0fe2f8424fe451f971303a732a7eca711e42
Frank Lehmann <frank.lehmann@bdr.de>
2023-02-02 14:17:39 +0100

    Initial Commit

---
+++ DependencyInjectionTest/DependencyInjectionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependencyInjectionTestApp.swift
+//  DependencyInjectionTest
+//
+//  Created by adm-P00800922 on 02.02.23.
+//
+
+import SwiftUI
+
+@main
+struct DependencyInjectionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
